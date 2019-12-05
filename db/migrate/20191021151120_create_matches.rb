a7ffbf06e43285bf1196a91aa7a24b9fb1e79fbd
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-12-04 18:16:36 -0600

    fixed float to decimal for lat and long

--- db/migrate/20191021151120_create_matches.rb
+++ db/migrate/20191021151120_create_matches.rb
@@ -2,9 +2,9 @@ class CreateMatches < ActiveRecord::Migration[6.0]
   def change
     create_table :matches do |t|
       t.integer :user_id
-      t.integer :place_id
-      t.float :place_lat
-      t.float :place_lng
+      t.string :place_id
+      t.decimal :place_lat
+      t.decimal :place_lng
       t.text :place_name
       t.boolean :favorite, default: false
 
